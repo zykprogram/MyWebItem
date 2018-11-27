f37331497e867b2302b4c15349405fa1e2b868b8
zykprogram <616975437@qq.com>
2018-11-27 13:51:01 +0800

    第一个项目测试SQLHelp类创建

---
+++ MyFirstItem/ADODemo/SQLHelp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADODemo
+{
+    public class SQLHelp
+    {
+        private static string GetStringConn()
+        {
+            return ConfigurationManager.ConnectionStrings["SqlConn"].ConnectionString;
+        }
+
+        public static int ExecuteNoQuery(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(GetStringConn()))
+            {
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    //打开连接
+                    conn.Open();
+                    //传入执行SQL
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }//end cmd
+            } //end conn
+        }
+
+        public static object ExecuteScalar(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(GetStringConn()))
+            {
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    //打开连接
+                    conn.Open();
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteScalar();
+                }//end cmd
+            }//end conn
+        }
+    }
+}
